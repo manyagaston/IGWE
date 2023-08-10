bed5b3f477a2792640cb3ba360b02bf8803424eb
Manya <junior.manya@groukam.com>
2023-08-10 21:36:18 +0100

    quiz

--- quizsite/quizsite/settings.py
+++ quizsite/quizsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '4a4c98%1zlm*nr6audnnfttl+iwte2i6g6uas9y$ir!s%13^)f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
-    'dj_rest_auth',
     
     'quiz',
 ]
